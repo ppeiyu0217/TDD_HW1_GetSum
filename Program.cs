6fa9e90cba86c0439a1da94ee7ea60b0e7c46100
ppeiyu0217 <ppeiyu0217@gmail.com>
2017-05-26 10:18:17 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetSum
+{
+   public class Program : ISum
+    {
+        public List<int> GetCostByCutNum(int CutNumber)
+        {
+            List<Data> DataLists = new List<Data>()
+            {
+                new Data {Id=1,Cost= 1,Revenue=11,SellPrice=21},
+                new Data {Id=2,Cost= 2,Revenue=12,SellPrice=22},
+                new Data {Id=3,Cost= 3,Revenue=13,SellPrice=23},
+                new Data {Id=4,Cost= 4,Revenue=14,SellPrice=24},
+                new Data {Id=5,Cost= 5,Revenue=15,SellPrice=25},
+                new Data {Id=6,Cost= 6,Revenue=16,SellPrice=26},
+                new Data {Id=7,Cost= 7,Revenue=17,SellPrice=27},
+                new Data {Id=8,Cost= 8,Revenue=18,SellPrice=28},
+                new Data {Id=9,Cost= 9,Revenue=19,SellPrice=29},
+                new Data {Id=10,Cost=10,Revenue=20,SellPrice=30},
+                new Data {Id=11,Cost=11,Revenue=21,SellPrice=31},
+            };
+            var Source = DataLists.Select(r => r.Cost);
+
+            return Calculate(Source, CutNumber);
+        }
+
+        public List<int> GetRevenueByCutNum(int CutNumber)
+        {
+            List<Data> DataLists = new List<Data>()
+            {
+                new Data {Id=1,Cost= 1,Revenue=11,SellPrice=21},
+                new Data {Id=2,Cost= 2,Revenue=12,SellPrice=22},
+                new Data {Id=3,Cost= 3,Revenue=13,SellPrice=23},
+                new Data {Id=4,Cost= 4,Revenue=14,SellPrice=24},
+                new Data {Id=5,Cost= 5,Revenue=15,SellPrice=25},
+                new Data {Id=6,Cost= 6,Revenue=16,SellPrice=26},
+                new Data {Id=7,Cost= 7,Revenue=17,SellPrice=27},
+                new Data {Id=8,Cost= 8,Revenue=18,SellPrice=28},
+                new Data {Id=9,Cost= 9,Revenue=19,SellPrice=29},
+                new Data {Id=10,Cost=10,Revenue=20,SellPrice=30},
+                new Data {Id=11,Cost=11,Revenue=21,SellPrice=31},
+            };
+            var Source = DataLists.Select(r => r.Revenue);
+
+            return Calculate(Source, CutNumber);
+        }
+
+        private List<int> Calculate(IEnumerable<int> Source, int CutNum)
+        {
+            int i = 0;
+            var result = new List<int>();
+            for (i = 0; i <= Source.Count() / CutNum; i++)
+            {
+                result.Add(Source.Skip(i * CutNum).Take(CutNum).Sum());
+            }
+            return result;
+        }
+    }
+
+    public interface ISum
+    {
+        List<int> GetCostByCutNum(int CutNumber);
+
+        List<int> GetRevenueByCutNum(int CutNumber);
+    }
+
+    public class Data
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
+    }
+}
---
+++ ProgramTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GetSum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+
+namespace GetSum.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void GetCostByCutNumTest()
+        {
+            //Arrange:
+            ISum getsumbycut3 = Substitute.For<ISum>();
+            getsumbycut3.GetCostByCutNum(3).Returns(new List<int> { 6, 15, 24, 21 });
+
+            //期望值
+            var expected = new List<int>{ 6, 15, 24, 21 };
+
+            //Act
+            var actual = getsumbycut3.GetCostByCutNum(3);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetRevenueByCutNumTest()
+        {
+            //Arrange
+            ISum getsumbycut4 = Substitute.For<ISum>();
+            getsumbycut4.GetRevenueByCutNum(4).Returns(new List<int> { 50, 66, 60 });
+
+            //期望值
+            var expected = new List<int> { 50, 66, 60 };
+
+            //Act
+            var actual = getsumbycut4.GetRevenueByCutNum(4);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
